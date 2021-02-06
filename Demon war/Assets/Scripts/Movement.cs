b8294fed30ae26b221747bd47b61568949d0f2f8
jerry0820 <chen3z3@mail.uc.edu>
2021-02-06 17:21:12 -0500

    Add game sample

---
+++ Demon war/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+    public Animator animator;
+
+    private Rigidbody2D _rigibody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigibody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (Input.GetButtonDown("Jump")&& Mathf.Abs(_rigibody.velocity.y) <0.001f)
+        {
+            _rigibody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
