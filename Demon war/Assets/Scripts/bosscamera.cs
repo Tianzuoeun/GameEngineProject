d73792d6c8e646e4d23f3214286c284dae20dd81
jerry0820 <chen3z3@mail.uc.edu>
2021-02-27 12:31:27 -0500

    update

---
+++ Demon war/Assets/Scripts/bosscamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bosscamera : MonoBehaviour
+{
+    public Animator camAnim;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            camAnim.SetBool("cutscene1", true);
+            Invoke(nameof(StopCutscene), 3f);
+        }
+    }
+    void StopCutscene()
+    {
+        camAnim.SetBool("cutscene1", false);
+    }
+}
